73d9309e7a963f4bd4d326179b81a947afcea091
tinnguyen69 <25825527+tinnguyen69@users.noreply.github.com>
2026-06-25 21:42:11 +0700

    feat: update 1st script

--- scripts/1_install_dependencies.sh
+++ scripts/1_install_dependencies.sh
@@ -2,8 +2,7 @@
 
 sudo dpkg --add-architecture i386
 sudo apt-get update
-sudo apt-get install libstdc++6:i386 libgcc1:i386 libcurl4-gnutls-dev:i386
-sudo apt-get install awscli -y
+sudo apt-get install libstdc++6:i386 libgcc1:i386 libcurl4-gnutls-dev:i386 awscli -y
 
 # Install steamcmd
 steamcmd_dir="$HOME/steamcmd"
